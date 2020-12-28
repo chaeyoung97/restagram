bd1cc0f750d469d10f553b72104b06d928727232
KBY-TECH <67587446+KBY-TECH@users.noreply.github.com>
2020-12-28 23:58:13 +0900

    Delete UserRepository.java

--- src/main/java/com/example/restagram/domain/Users/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.restagram.domain.Users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-}
