522b9c3f54824d5205e058790cce358e02a0ef1b
niveus323 <niveus323@gmail.com>
2020-12-31 05:09:15 +0900

    이미지 서비스 함수 완성

--- src/main/java/com/example/restagram/domain/images/ImagesRepository.java
+++ src/main/java/com/example/restagram/domain/images/ImagesRepository.java
@@ -1,7 +1,13 @@
 package com.example.restagram.domain.images;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImagesRepository extends JpaRepository<Images, Long>{
-
+	Images findByUserId(Long userId);
+	Images findByPostId(Long postId);
+	List<Images> findAllByPostId(Long userId);
+	boolean existsByUserId(Long userId);
+	boolean existsByPostId(Long postId);
 }
