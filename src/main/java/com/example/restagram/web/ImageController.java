860b5d321d32ffb5afd35f41457665e78b91cfeb
chaeyoung <codud7239@gmail.com>
2021-01-03 07:46:11 +0900

    [update] css,이미지 path 변경 및 모든 css,이미지 참조 경로 변경

--- src/main/java/com/example/restagram/web/ImageController.java
+++ src/main/java/com/example/restagram/web/ImageController.java
@@ -3,7 +3,6 @@ package com.example.restagram.web;
 import java.io.IOException;
 import java.util.List;
 
-import com.example.restagram.domain.posts.Posts;
 import com.example.restagram.domain.posts.PostsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
