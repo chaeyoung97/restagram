c9b6a91a91bfc69e5620d5d5a60a9f1db7b09221
KBY-TECH <soccer_by@naver.com>
2021-01-02 23:52:57 +0900

    controller commit

--- src/main/java/com/example/restagram/web/IndexController.java
+++ src/main/java/com/example/restagram/web/IndexController.java
@@ -5,10 +5,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+    // 기본 home
     @GetMapping("/")
     public String index()
     {
         return "index";
+    }
+
+    //회원가입 페이지
+    @GetMapping("/signUpForm")
+    public String signUpForm() {
+        return "user/signUpForm";
+    }
+
+    //로그인 페이지로 이동
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "/user/login";
+    }
+
+    //팔로우
+    @GetMapping("/follow")
+    public String follow() {
+        return "";
+    }
+
+    //팔로워
+    @GetMapping("/follower")
+    public String follower() {
+        return "";
     }
 
 }
