929e1c81f5fe64ccdab14697a578397c56ca539c
KBY-TECH <soccer_by@naver.com>
2021-01-05 23:52:36 +0900

    chatRoom 임시테이블 생성 및 navigator.html 수정 signUpForm.html 수정

--- src/main/java/com/example/restagram/web/IndexController.java
+++ src/main/java/com/example/restagram/web/IndexController.java
@@ -21,13 +21,11 @@ public class IndexController {
     private final UsersRepository usersRepository;
     private final UserService userService;
     private final ImageService imageService;
-    
+
     // 기본 home
     @GetMapping("/")
-    public String index(Model model, @LoginUser SessionUser sessionUser, @AuthenticationPrincipal PrincipalDetail userDetails)
-    {
-        if(sessionUser == null)
-        {
+    public String index(Model model, @LoginUser SessionUser sessionUser, @AuthenticationPrincipal PrincipalDetail userDetails) {
+        if (sessionUser == null) {
             return "login"; //로그인이 되어있지 않다면 로그인화면으로 이동
         }
         model.addAttribute("login", userDetails.getUser());
@@ -48,15 +46,13 @@ public class IndexController {
 
     // 관리자만 허용하는 userList
     @GetMapping("/admin/userList")
-    public String userList(@LoginUser SessionUser user,Model model){
+    public String userList(@LoginUser SessionUser user, Model model) {
         System.out.println("여기가 관리자 페이지."); // 관리자의 역할  h2 데이터 베이스 직접 변경
-                                                    // update users set ROLE='ADMIN' where username='admin'
+        // update users set ROLE='ADMIN' where username='admin'
         try {
             user.getUsername(); // null 이 발생하면 예외처리 구문으로 Error를 일으킴.
-            model.addAttribute("userList",userService.userlist());
-        }
-        catch (Exception e)
-        {
+            model.addAttribute("userList", userService.userlist());
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return "/user/userList";
@@ -64,16 +60,16 @@ public class IndexController {
 
     //게시물 작성 페이지로 이동
     @GetMapping("/posts")
-    public String write(@LoginUser SessionUser sessionUser){
-        if(sessionUser == null)
+    public String write(@LoginUser SessionUser sessionUser) {
+        if (sessionUser == null)
             return "login";//아직 예외 페이지 처리가 안되어 있어 일단 로그인 페이지로 가도돍 함;
         return "new_post";
     }
 
     //프로필 페이지로 이동
     @GetMapping("/profile")
-    public String profile(@LoginUser SessionUser sessionUser, Model model){
-        if(sessionUser == null)
+    public String profile(@LoginUser SessionUser sessionUser, Model model) {
+        if (sessionUser == null)
             return "login";
         Users users = usersRepository.findByUsername(sessionUser.getUsername()).get();
         model.addAttribute("session", users);
