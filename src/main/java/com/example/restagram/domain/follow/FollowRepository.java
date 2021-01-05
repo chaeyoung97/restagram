724ba05b20eccaee61a64b9dd8c702dc713eb0c6
sujin7837 <31465594+sujin7837@users.noreply.github.com>
2021-01-05 22:38:01 +0900

    followController_test_ing

--- src/main/java/com/example/restagram/domain/follow/FollowRepository.java
+++ src/main/java/com/example/restagram/domain/follow/FollowRepository.java
@@ -7,7 +7,7 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 public interface FollowRepository extends JpaRepository<Follow, Long>{
-    @Transactional
+
     Long deleteByFromUserIdAndToUserId(Long fromUser, Long toUser);
     List<Follow> findByFromUser(Long id);
     List<Follow> findByToUser(Long id);
